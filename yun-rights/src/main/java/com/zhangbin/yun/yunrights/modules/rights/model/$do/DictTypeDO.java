12fa76e93ff8b712e19fbc41ffac637b4820e7cd
huolianchiyun <784831631@qq.com>
2020-09-15 16:50:32 +0800

    优化字典管理

---
+++ yun-rights/src/main/java/com/zhangbin/yun/yunrights/modules/rights/model/$do/DictTypeDO.java
@@ -0,0 +1,40 @@
+package com.zhangbin.yun.yunrights.modules.rights.model.$do;
+
+import com.zhangbin.yun.yunrights.modules.common.model.$do.BaseDo;
+import java.io.Serializable;
+import com.zhangbin.yun.yunrights.modules.rights.model.common.NameValue;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 字典类型表
+ * 表 t_sys_dict_type
+ * @author ASUS
+ * @date 2020-09-15 10:20:49
+ */
+@Getter
+@Setter
+public class DictTypeDO extends BaseDo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 显示名
+     */
+    private String name;
+
+    /**
+     * 类型编码
+     */
+    private String code;
+
+    /**
+     */
+    private String description;
+
+    /**
+     */
+    private Integer sort;
+
+    public NameValue<String> toNameValue(){
+        return new NameValue<>(name, code);
+    }
+}
